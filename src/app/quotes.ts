e2d476b1330b2c9d435b68eaf618e35ae6b61995
Raphael Katana <roduor41@gmail.com>
2018-10-19 16:24:05 +0300

    add  class and quote component, added the initial quotes

---
+++ src/app/quotes.ts
@@ -0,0 +1,6 @@
+export class Quotes {
+  quotes:string[];
+   constructor(public quote:string, public author:string, public votes:number){
+
+  }
+}
---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import{Quotes} from '../quotes';
+
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  quotes=[
+    new Quotes("Old is gold","Katana",0),
+    new Quotes("What goes around, comes around","Katana",0),
+    new Quotes("Not all that glitters is gold","Katana",0),
+    new Quotes("Better late than never","Katana",0),
+    new Quotes("Coding is toothpaste","Katana",0),
+  ]
+}
